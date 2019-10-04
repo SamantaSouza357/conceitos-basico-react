b744e4676281392045966912cb0bd19769a6be9a
samantasouza357 <samantasouza931@gmail.com>
2019-10-04 15:20:58 -0300

    funcionalidade

--- src/componentes/Placar.js
+++ src/componentes/Placar.js
@@ -4,13 +4,25 @@ import  "./Placar.scss";
 const Placar = ({time}) =>{
     const [votos, setVotos] = useState(0);
 
+    const incrementar = ()  => {
+        setVotos(votos + 1);
+    }
+    const descrementar = () => {
+       if (votos >= 1) {
+           setVotos(votos - 1 );
+       }
+       else{
+           alert('impossivel diminuir');
+       }
+    }
+
     return (
         <div className="Placar">
             <h1>{time} </h1>
             <h2>{votos}</h2>
             <div>
-                <button>-</button>
-                <button>+</button>
+                <button onClick={descrementar}>-</button>
+                <button onClick={incrementar}>+</button>
             </div>
         </div>
     )
